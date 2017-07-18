39aff12e8f7446615e61db448dbcf9b43d9bbba5
Fu xiaotong <Fu xiaotong>
2017-07-19 07:54:41 +0800

    init

---
+++ JS/Script.js
@@ -0,0 +1,17 @@
+
+	
+function showDialog(id){	
+	document.getElementById(id).style.display = 'inline';
+}	
+
+function closeDialog(id){	
+	document.getElementById(id).style.display = 'none';
+}	
+
+/*
+window.onload=function(){
+    document.onmousedown=function(ev){
+        document.getElementById("NOIP").style.display = 'none';
+		alert(document.getElementById("NOIP").offsetX);
+    }
+}*/
